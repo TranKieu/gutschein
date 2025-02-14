868d3e79a674334ca7fc4a64e28ffab3a7289a65
trankieu.io <tranvd2010@gmail.com>
2025-02-14 12:30:26 +0100

    first commit

--- apps/maicafe/eslint.config.mjs
+++ apps/maicafe/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'maicf',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'maicf',
           style: 'kebab-case',
         },
       ],
